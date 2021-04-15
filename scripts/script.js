86d5e05d34120281922aafe514d2c7a076bcdf34
omgwer <hersgory2134@gmail.com>
2021-04-15 19:02:37 +0300

    upd

--- scripts/script.js
+++ scripts/script.js
@@ -2,40 +2,71 @@ let calcDisplay = document.querySelector('.calculator_display');
 let calcButtons = document.querySelector('.calculator_buttons');
 let calcButton = document.querySelectorAll('.calculator_button');
 let selectorOperation = false;
-res = []
-
+let firstOperand  = '';
+let secondOperand = '';
+let operand = '0';
 function updateDisplay(info){
   calcDisplay.textContent += info;
 }
 
 function clearDisplay(){
   calcDisplay.textContent = '';
+
 }
 
 function getResult(res) {
-  let result = eval(res);
+  //let result = eval(res).toFixed(3);
   calcDisplay.textContent += ' = ' + result;
   selectorOperation = true;
-
 }
 
+function numberSelector(selector) {
+  if (selector == '+' ||
+    selector == '-' ||
+    selector == '*' ||
+    selector == '/') {
+    operand = selector;
+
+  } else {
+    if (operand !== '0') {
+      secondOperand += selector;
+    } else {
+      firstOperand += selector;
+    }
+  }
+}
+function result() {
+  if (operand == '-') {
+    return  firstOperand - secondOperand;
+  }
+  if (operand == '+') {
+    return -(-firstOperand - secondOperand);
+  }
+  if (operand == '/') {
+    return firstOperand / secondOperand;
+  }
+  if (operand == '*') {
+    return firstOperand * secondOperand;
+  }
+}
 
 for (let i = 0; i < calcButton.length; i++) {
         calcButton[i].addEventListener('click', function () {
-
           if (selectorOperation) {
             clearDisplay();
-            console.log('clear!');
             selectorOperation = false;
+            res = [];
           }
           if (calcButton[i].textContent == '=') {
-            getResult(res);
-
+            calcDisplay.textContent = result();
           } else {
-
-            res += calcButton[i].textContent;
-            console.log(res);
+            numberSelector(calcButton[i].textContent);
             updateDisplay(calcButton[i].textContent);
+            console.log(firstOperand);
+            console.log(secondOperand);
+            console.log(operand);
+
+
           }
 
         });
